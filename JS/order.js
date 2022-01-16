4d5753b640e77a258302a53785b00a814b6a6d75
Nyanjuimarvin <marnyanjui@gmail.com>
2022-01-16 11:06:05 +0300

    Add plus and minus button logic

--- JS/order.js
+++ JS/order.js
@@ -1,1 +1,32 @@
+$(document).ready(() => {
+  
+  //plus button logic
+  $(".plus-btn").on("click", function (e) {
+    e.preventDefault();
+    let quantity = $(this).closest("div").find("input");
+    let value = parseInt(quantity.val());
 
+    if (value < 100) {
+      value = value + 1;
+    } else {
+      value = 1;
+    }
+
+    quantity.val(value);
+  });
+
+  //minus button logic
+  $(".minus-btn").on("click", function (e) {
+    e.preventDefault();
+    let quantity = $(this).closest("div").find("input");
+    let value = parseInt(quantity.val());
+
+    if (value > 1) {
+      value = value - 1;
+    } else {
+      value = 0;
+    }
+
+    quantity.val(value);
+  });
+});
