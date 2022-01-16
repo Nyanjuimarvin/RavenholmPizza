41f4899adda0a81b301e6e94e6c10f20bcffb5e3
Nyanjuimarvin <marnyanjui@gmail.com>
2022-01-16 20:59:20 +0300

    Add function for checkboxes

--- JS/order.js
+++ JS/order.js
@@ -1,31 +1,113 @@
-class Pizza{
-  constructor(size, crust, toppings, name,quantity) {
-		this.size = size;
-		this.crust = crust
-		this.toppings = toppings;
-		this.name = name;
+class Pizza {
+  constructor(size, crust, toppings, name, quantity) {
+    this.size = size;
+    this.crust = crust;
+    this.toppings = toppings;
+    this.name = name;
     this.quantity = quantity;
-	}
-
-  currentOrder(){
-    const { size,crust,toppings,name,quantity } = this;
-    return `Name:${name} ,Size:${size},Quantity:${quantity}, Crust:${crust}, Toppings:${toppings}`;
   }
 
-  currentPrice(){
-    const { size,crust,toppings,quantity } = this;
-    return (size + crust + toppings ) * quantity;
+  splitToppings() {
+    const { toppings } = this;
+    for (let top of toppings) {
+      return top;
+    }
+  }
+  currentOrder() {
+    const { size, crust, toppings, name, quantity } = this;
+    return `Name:${name} ,Size:${size},Quantity:${quantity}, Crust:${crust}, Toppings:${this.splitToppings()}`;
   }
 
-  orderSummary(){
+
+  buildPrice() {
+    const { size } = this;
+    let crustPrice = 0;
+    let toppingsPrice = 0;
+    let sizePrice = 0;
+
+    if (size === "mega") {
+      crustPrice = crustArray[0];
+      toppingsPrice = toppingsArray[0];
+      sizePrice = sizeArray[0];
+    } else if (size === "large") {
+      crustPrice = crustArray[1];
+      toppingsPrice = toppingsArray[1];
+      sizePrice = sizeArray[1];
+    } else if (size === "medium") {
+      crustPrice = crustArray[2];
+      toppingsPrice = toppingsArray[2];
+      sizePrice = sizeArray[2];
+    } else if (size === "small"){
+      crustPrice = crustArray[3];
+      toppingsPrice = toppingsArray[3];
+      sizePrice = sizeArray[3];
+    }else{
+      crustPrice = null;
+      toppingsPrice = null;
+      sizePrice = null;
+    }
+
+    return crustPrice + toppingsPrice + sizePrice;
+  }
+
+  toppingsPrice() {
+    const { toppings } = this;
+    return (
+      toppings.length *
+      toppingsArray.reduce((total, initial) => total + initial)
+    );
+  }
+  currentPrice() {
+    const { size, crust, quantity } = this;
+    return (size + crust + this.toppingsPrice()) * quantity;
+  }
+
+  orderSummary() {
     return `${this.currentOrder}  ${this.currentPrice}`;
   }
-
 }
+
+const toppingsArray = [100, 80, 60, 40];
+const crustArray = [900, 720, 540, 360];
+const sizeArray = [400, 300, 200, 100];
+
+//Array to push currentPrice
+const totalArray = [];
+
+//Array to push currentOrder
+const allOrders = [];
+
+//function to check if .checked === true
+const pizzaToppings = ()=> {
+  const nodeList = document.getElementsByClassName('toppings');
+  const arr = [];
+
+  for (let i of nodeList) {
+    if (i.checked !== false) arr.push(i.value);
+  }
+  return arr;
+}
+
+//form elements
+const pizzaName = document.querySelector("#pizzas");
+const pizzaSize = document.querySelector("#sizes");
+const pizzaCrust = document.querySelector("#crusts");
+const pizzaQuantity = document.querySelector(".qty");
 
 
 $(document).ready(() => {
+  $(".orderform").submit(function (e) {
+    e.preventDefault();
+    $(".form1").hide();
+    $(".form2").show();
+  });
 
+  $("#addpizza").click((e) => {
+    $(".orderform")[0].reset();
+  });
+});
+
+$(document).ready(() => {
   //plus button logic
   $(".plus-btn").on("click", function (e) {
     e.preventDefault();
